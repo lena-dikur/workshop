b783f4d9f1fec08e05479df5b1afde9ffa3e0e4d
oesel <oesel@yandex.ru>
2022-12-04 21:28:02 +0300

    added working folder Sem001_5

---
+++ Seminar001_5/Program.cs
@@ -0,0 +1,9 @@
+﻿//Считываем данные с консоли 
+string? imputNumLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if(imputNumLine!=null)
+{
+    int imputNumber = int.Parse(imputNumLine);
+    int imputNumberC = imputNumber % 10;
+    Console.WriteLine(imputNumberC);
+}
