9404cfe75539b8ff32ad565acc7fe074b64445b6
oesel <oesel@yandex.ru>
2022-12-04 21:25:38 +0300

    added working folder Sem001_4

---
+++ Seminar001_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Считываем данные с консоли 
+string? imputLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if(imputLine!=null)
+{
+// Создаем массив с днями недели
+    string[] dayofWeek = new string[7];
+    dayofWeek[0] = "понедельник";
+    dayofWeek[1] = "вторник";
+    dayofWeek[2] = "среда";
+    dayofWeek[3] = "четверг";
+    dayofWeek[4] = "пятница";
+    dayofWeek[5] = "суббота";
+    dayofWeek[6] = "воскресение";
+// Парсим введенное число
+    int imputNumber = int.Parse(imputLine);
+// Находим название дня недели по введенному номеру
+    string outDayofWeek = dayofWeek[imputNumber-1];
+// Выводим данные в консоль
+    Console.WriteLine(outDayofWeek);
+}
+
+
+
