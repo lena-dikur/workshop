dd860d6fbcb4a5f929bff30b2e21bc39a7f3d7f9
oesel <oesel@yandex.ru>
2022-12-04 21:24:16 +0300

    added working folder Sem001_2

---
+++ Seminar001_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Считываем данные с консоли
+string? num1Line = Console.ReadLine();
+// Проверяем чтобы данные были не пустыми
+if(num1Line!=null)
+{
+// Парсим введенное число
+// int num1 = int.Parse(num1Line);
+// Находим квадрат числа
+// int outResult = num1 * num1;
+// int outnum1 = (int)Math.Pow(num1, 2);  
+// Выводим результат в консоль
+//  Console.WriteLine(outnum1);
+    Console.WriteLine(Math.Pow(int.Parse(num1Line),2));
+}
