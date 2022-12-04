9fee352b90b330cc44125c459ac8b02999a6aea3
oesel <oesel@yandex.ru>
2022-12-04 21:26:16 +0300

    added working folder Sem001_4.1

---
+++ Seminar001_4.1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Считываем данные с консоли 
+string? imputLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if(imputLine!=null)
+{
+    // Парсим введенное число
+    int imputNumber = int.Parse(imputLine);
+    string outDayofWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(imputNumber));
+    // Выводим данные в консоль
+    Console.WriteLine(outDayofWeek);
+}
+
