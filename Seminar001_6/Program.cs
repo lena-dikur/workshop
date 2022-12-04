94d81e9cf59d573c39ea5694e349a12924a1e633
oesel <oesel@yandex.ru>
2022-12-04 21:28:27 +0300

    added working folder Sem001_6

---
+++ Seminar001_6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Считываем данные с консоли 
+string? imputNumLine = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if(imputNumLine!=null)
+{
+// Парсим введенное число
+    int imputNumberA = int.Parse(imputNumLine);
+// Вводим промежуточное значение
+    int imputNumberC = imputNumberA*(-1);
+// Выходные значения
+    string outNumLine = string.Empty;
+// 
+    while(imputNumberC < imputNumberA)
+    {
+        outNumLine = outNumLine + imputNumberC + ",";
+        imputNumberC = imputNumberC + 1;
+    }
+    outNumLine = outNumLine + imputNumberA;
+    Console.WriteLine(outNumLine);
+}
